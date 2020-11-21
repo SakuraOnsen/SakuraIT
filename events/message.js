e595a08f8fd5543ec46a88c4cb7a1b1ed6a9a7fb
Junior <hraklhs.klm@gmail.com>
2020-11-21 12:56:13 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,6 +4,5 @@ module.exports = async (sakura, message) => {
     const commandName = await args.shift().toLowerCase();
     const command = sakura.commands.get(commandName) || sakura.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     if (!command) return;
-    if (message.author.id != "379267610564362243" && message.author.id != "301616576313032715") return;
     command.execute(sakura, message, args);
 }
